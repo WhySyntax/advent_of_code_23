8c19b1ebc6d3ae8773c9e59596d477207de5d09c
Kabir Vidyarthi <87249903+WhySyntax@users.noreply.github.com>
2023-12-04 21:05:44 -0800

    started on day 3 and added comments to day 2

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,36 @@ use std::cmp;
 
 fn main() {
     let da_text = fs::read_to_string("input.txt").unwrap();
-    println!("{}",_day_one(da_text));
+    println!("{}",day_three(da_text));
+}
+
+fn day_three (input:String) -> usize {
+    let cleaned:String = input.chars()
+        .filter(|&x| x != '\n') // makes entire input into a single string for me to clean up later
+        .map(|x| {
+            if (('0' >= x) || ('9' <= x)) && (x != '.') {
+                'a' // makes special characters more easily identifiable
+            } else {x}
+        }).collect();
+    let mut total = 0;
+    let mut nums:Vec<[usize;2]> = Vec::new();
+    return total;
 }
 
 fn _day_two(input:String) -> usize {
     let colors = ["red","green","blue"];
-    let da_textier:Vec<&str> = input.split("\nGame ").map(|x| x.trim().split(": ").last().unwrap()).collect();
+    let da_textier:Vec<&str> = input.split("\nGame ").map(|x| x.trim().split(": ").last().unwrap()).collect(); // separates each game's input
     let mut total = 0;
 
     for i in 0..da_textier.len() {
-        let games:Vec<&str> = da_textier[i].split("; ").collect();
+        let games:Vec<&str> = da_textier[i].split("; ").collect(); // removes game # because I can use the loop index for that
         let mut mins:[usize;3] = [0,0,0]; 
         for j in games.iter() {
-            let aaahhhh:String = j.chars().filter(|&x| x != ',').collect();
-            let game:Vec<&str> = aaahhhh.split(' ').collect();
+            let aaahhhh:String = j.chars().filter(|&x| x != ',').collect(); // removes the commas so I don't have to deal with them when parsing nums
+            let game:Vec<&str> = aaahhhh.split(' ').collect(); // separates nums from colors
             let mut exists:Option<usize>;
             for k in 0..3 {
-                exists = game.iter().position(|&x| x == colors[k]);
+                exists = game.iter().position(|&x| x == colors[k]); // checks if a color is used and puts the amount in mins if needed
                 if exists.is_some() {
                     let amount = game[exists.unwrap() - 1].parse::<usize>().unwrap();
                     mins[k] = cmp::max(amount, mins[k]);
@@ -36,7 +49,7 @@ fn _day_one(input:String) -> u32 {
     let mut da_text= input;
     let mut da_textier:Vec<char> = da_text.chars().collect();
     let num_words = ["zero","one","two","three","four","five","six","seven","eight","nine"];
-    for i in 0..10 {
+    for i in 0..10 { // makes the 2nd letter of each digit word into the digit itself
         let mut loc = da_text.find(num_words[i]);
         while loc.is_some() {
             da_textier[loc.unwrap() + 1] = char::from_digit(i as u32, 10).unwrap();
